49377ba89e4475c15c0cb7cb747b2165a5b8f39b
George Zografos <georgios@secfi.com>
2023-09-15 22:15:08 +0200

    fix script

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -1,6 +1,6 @@
 aws dynamodb update-item \
     --table-name data \
     --key '{"PK": { "S": "API_KEY#secret" }, "SK": { "S": "API_KEY#secret" } }' \
-    --attribute-updates '{"VALUE": { "Value": { "S": "API_KEY#secret" } } }' \
+    --attribute-updates '{"VALUE": { "Value": { "S": "{\"id\": 5, \"name\": \"George\"}" } } }' \
     --endpoint-url http://localhost:8000 \
     --region local
